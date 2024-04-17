cb76a3f6b96de5cc18fbabd4c07cbba6bacb580b
temirkhanovams <marinchik.t.s@gmail.com>
2024-04-17 17:15:24 +0300

    отформатировала conftest на ветке main

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,34 +23,8 @@ def setup_browser(request):
     browser.config.window_height = 780
     driver_options = webdriver.ChromeOptions()
 
-    # driver_options = ChromeOptions()
-
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
-
-    # browser_name = request.config.getoption('--browser_name')
-    # browser_version = request.config.getoption('--browser_version')
-    # browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    # browser_name = 'chrome'
-    # driver_options = ChromeOptions()
-    # driver_options.page_load_strategy = 'eager'
-    # browser.config.driver_options = driver_options
-    # browser.config.base_url = "https://demoqa.com"
-    # browser.config.window_width = 1920
-    # browser.config.window_height = 1080
-
-    # selenoid_capabilities = {"browserName": browser_name, "browserVersion": browser_version,
-    #                          "selenoid:options": {"enableVNC": True, "enableVideo": True}}
-    #
-    # driver_options.capabilities.update(selenoid_capabilities)
-
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-    #     options=driver_options)
-    #
-    # browser.config.driver = driver
 
     yield browser
     with allure.step('Add screenshot'):
